462a3920922e71fcc581554c30bc5e725cbeaf4c
ps-vitor <the-jungleman@proton.me>
2025-04-18 13:59:02 -0300

    created server function and returning to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  axios from  'axios';
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    axios.get('https://localhost:5000/api/ping')
+      .then(res=>console.log(res.data))
+      .catch(err=>console.error(err));
+  },[])
 
   return (
     <>
---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import  express from    'express';
+import  cors    from    'cors';
+import  dotenv  from    'dotenv';
+
+function    main(){
+    dotenv.config();
+
+    const   app=express();
+    app.use(cors());
+    app.use(express.json());
+
+    app.get('/api/ping',(_req,res)=>{
+        res.json({message:'pong'});
+    });
+
+    const   PORT=process.env.PORT||5000;
+    app.listen(PORT,()=>{
+        console.log(`Server running on port ${PORT}`)
+    })
+}
