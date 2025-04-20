a8063345b5c5b74f2e7b2c9a5f083282dd6b995d
ps-vitor <the-jungleman@proton.me>
2025-04-20 00:52:14 -0300

    20/4/2025

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Filho da puta caralho arrombado</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
