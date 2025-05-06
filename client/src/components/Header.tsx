ebea938a1c48b793ee6ea792dcb1b1ed4e26fc42
ps-vitor <the-jungleman@proton.me>
2025-05-06 19:14:58 -0300

    fixed button

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import singulaLogo from '/images/singula.jpg'
 import  styles  from  '../styles/Header.module.css'
+import  {FaBars}   from    'react-icons/fa';
 
 export  default function    Header(){
-    const[open,setOpen]=useState(false) 
+    const[open,setOpen]=useState(false)
 
     const toggleMenu=()=>{
       setOpen(prev=>!prev)
@@ -12,16 +13,17 @@ export  default function    Header(){
 
     return(
         <header className={styles.header}>
-            <Link   to="/">
+            <Link   to="/"  onClick={()=>setOpen(false)}>
             <img src={singulaLogo} className={styles.logo} alt="Singula logo" />
             </Link>
             <div    className={styles.menuContainer}>
-                <button className={styles.menuButton} onClick={toggleMenu}>☰</button>
+                <button className={styles.menuButton} onClick={toggleMenu}><FaBars />
+                </button>
                 {open && (
                   <ul className={styles.dropdown}>
-                    <li><Link to="/">Início</Link></li>
-                    <li><Link to="/aulas">Aulas</Link></li>
-                    <li><Link to="/artigos">Artigos</Link></li>
+                    <li><Link to="/"  onClick={toggleMenu}>Início</Link></li>
+                    <li><Link to="/aulas" onClick={toggleMenu}>Aulas</Link></li>
+                    <li><Link to="/artigos" onClick={toggleMenu}>Artigos</Link></li>
                   </ul>
                 )}
             </div>
